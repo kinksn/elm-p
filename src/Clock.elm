46a9c2a6bd80ee75e733d6f38f1afe737ab8d9d4
shunya kataoka <shunya_kataoka@fringe81.com>
2021-02-06 00:32:22 +0900

    とけいつくった

--- src/Clock.elm
+++ src/Clock.elm
@@ -9,10 +9,18 @@ module Clock exposing (..)
 繰り返します！
 
 
-▼作るアプリケーション：ボタンを押すとランダムに猫の画像を表示する
+▼作るアプリケーション：現在時刻を1秒ごとに更新する(hh:mm:ss)
+- アプリケーションで時間を表現するには「POSIX時間」と「タイムゾーン」が必要
+- hh:mm:ss の形式で表示
 
 - [MODEL]
+- POSIX時間とタイムゾーンを持たせる
+- zone : Time.Zone, time : Time.Posix
 
+
+- [UPDATE]
+- モデルのPOSIX時間とタイムゾーンを更新するためのMsg
+- 
 --}
 
 import Browser
@@ -22,8 +30,8 @@ import Time
 
 
 
--- MAIN mianはElmでは特別な値で、画面に何を表示するかを記述します
 
+-- MAIN mianはElmでは特別な値で、画面に何を表示するかを記述します
 
 main =
   Browser.element
@@ -37,6 +45,16 @@ main =
 
 -- MODEL modelはアプリケーションが持つデータを表現している、initは初期状態の定義
 
+type alias Model =
+  { zone : Time.Zone
+  , time : Time.Posix
+  }
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model Time.utc (Time.millisToPosix 0 )
+  , Task.perform AdjustTimeZone Time.here
+  )
 
 
 
@@ -44,17 +62,40 @@ main =
 -- 操作したい内容は Msgというカスタム型を作り、その中で定義するのが慣例
 -- update関数はMsgとModelを受け取り、新しいモデルを返す
 
--- ボタンを押すとランダムに猫画像を取得する
--- gif画像を取得する
+type Msg
+  = Tick Time.Posix
+  | AdjustTimeZone Time.Zone
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    Tick newTime ->
+      ( { model | time = newTime }, Cmd.none )
+
+    AdjustTimeZone here ->
+      ( { model | zone = here }, Cmd.none )
 
 
 
 
 -- SUBSCRIPTIONS
 
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  Time.every 1000 Tick
 
 
 
 
 -- VIEW view関数は画面にModelの内容を表示する関数
 -- view関数はModelを受け取り、Html msgを返す
+
+view : Model -> Html Msg
+view model =
+  let
+    hour = String.fromInt ( Time.toHour model.zone model.time )
+    minutes = String.fromInt ( Time.toMinute model.zone model.time )
+    second = String.fromInt ( Time.toSecond model.zone model.time )
+  in
+    h1 [] [ text ( hour ++ "：" ++ minutes ++ "：" ++ second ) ]
